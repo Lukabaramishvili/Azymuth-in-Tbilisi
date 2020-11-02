fb17a646c18efb2cda351e9d6e93a431d895ce10
github username <Lukabaramishvili@gmail.com>
2020-11-02 13:44:36 +0400

    add speakers & schedule section w/ styles

---
+++ main.js
@@ -0,0 +1,15 @@
+
+//we grab our header and our desktop header
+//we insert the contents of our header into the desktop one
+
+const header = document.querySelector('.header');
+const desktopHeader = document.querySelector('.header-desktop');
+
+desktopHeader.innerHTML = header.innerHTML;
+
+//1. when the header enters the viewport, hide the desktop header
+//2. when the header leaves, show it (by adding the visible class) 
+
+inView('.header')
+    .on('enter', el => desktopHeader.classList.remove('visible'))
+    .on('exit', el => desktopHeader.classList.add('visible'));
